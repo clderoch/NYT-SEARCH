f2299d5b306bea4646cd31cd254abdb7e06cf49d
Craig Derocher <craigderocher@Craig.local>
2018-11-03 13:14:08 -0500

    Message

---
+++ JavaScript/game.js
@@ -0,0 +1,17 @@
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+    'api-key': "43d849702e6a41b19baf083bc1d4f8e2",
+    'q': "Iran",
+    'begin_date': "20180801",
+    'end_date': "20180901"
+    var
+});
+$.ajax({
+    url: url,
+    method: 'GET',
+}).done(function (result) {
+    console.log(result);
+}).fail(function (err) {
+    throw err;
+});
